f1e05d0f8ff7309007c66ab58e4aa3d64ad6abb9
Rafael Vasconcelos <vasconceloss.raffa@gmail.com>
2023-05-10 22:09:10 -0300

    Frases

--- app/src/main/java/com/ds2/affirmations/MainActivity.kt
+++ app/src/main/java/com/ds2/affirmations/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.ds2.affirmations
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.ds2.affirmations.adapter.ItemAdapter
 import com.ds2.affirmations.data.Datasource
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView: TextView = findViewById(R.id.TextView)
-        textView.text = Datasource().loadAffirmations().size.toString()
+// Initialize data.
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+// Use this setting to improve performance if you know that changes
+// in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
     }
 }
